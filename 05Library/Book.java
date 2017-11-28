ebe53e5b09be00a21a2643e3fad83bbc64e1f42e
imad <ibelkebir@stuy.edu>
2017-11-28 15:23:40 -0500

    Added library.java and wrote book.java

---
+++ 05Library/Book.java
@@ -0,0 +1,19 @@
+public class Book{
+    private String author;
+    private String title;
+    private String isbn;
+    public Book(String author, String title, String isbn){
+	this.author = author;
+	this.title = title;
+	this.isbn = isbn;
+    }
+    public Book(){
+    }
+    public String getAuthor(){return author;}
+    public String getTitle(){return title;}
+    public String getISBN(){return isbn;}
+    public void setAuthor(String author){this.author = author;}
+    public void setTitle(String title){this.title = title;}
+    public void setISBN(String isbn){this.isbn = isbn;}
+    public String toString(){return title+", "+author+", "+isbn;}
+}
