1b3f9d9576ba45212f0b5a7883d43cf9474a620d
Imad <ibelkebir@stuy.edu>
2017-12-06 00:04:31 -0500

    updated methods

--- 07Barcode/Barcode.java
+++ 07Barcode/Barcode.java
@@ -2,16 +2,25 @@ public class Barcode{
     private String zip;
     private String barcode = "";
     public Barcode(String zip){
+	if(zip.length() != 5){
+	    throw new IllegalArgumentException();
+	}
+	for(int x=0; x<5; x++){
+	    if(!(Character.isDigit(zip.charAt(x)))){
+		throw new IllegalArgumentException();
+	    }
+	}
 	this.zip = zip;
-	makeBarcode(zip);
+	barcode = makeBarcode(zip);
     }
-    private void makeBarcode(String zip){
+    private static String makeBarcode(String zip){
 	int sum = 0;
-	for(int x=0; x<zip.length(); x++){
+	for(int x=0; x<5; x++){
 	    sum += Integer.parseInt(zip.substring(x,x+1));
 	}
-	zip += sum/zip.length();
-	for(int x=0; x<zip.length(); x++){
+	zip += sum % 10;
+	String barcode = "";
+	for(int x=0; x<6; x++){
 	    if(zip.charAt(x) == '0'){
 		barcode += "||:::";
 	    }
@@ -43,17 +52,90 @@ public class Barcode{
 		barcode += "|:|::";
 	    }
 	}
-	zip = zip.substring(0,zip.length()-1);
+	zip = zip.substring(0,5);
+	return barcode;
     }
     public String getZip(){return zip;}
     public String getCode(){return this.toString();}
-    public String toString(){return "|" + barcode + "|";}
+    public String toString(){return "|" + barcode + "| (" + zip + ")";}
     public int compareTo(Barcode other){return zip.compareTo(other.zip);}
     public boolean equals(Barcode other){
 	if(zip.compareTo(other.zip) == 0){
 	    return true;
 	}
 	return false;
+    }
+    public static String toCode(String zip){
+	if(zip.length() != 5){
+	    throw new IllegalArgumentException();
+	}
+	for(int x=0; x<5; x++){
+	    if(!(Character.isDigit(zip.charAt(x)))){
+		throw new IllegalArgumentException();
+	    }
+	}
+	return "|" + makeBarcode(zip) + "|";
+    }	
+    public static String toZip(String barcode){
+	if(barcode.length() != 32 || barcode.charAt(0) != '|' || barcode.charAt(31) != '|'){
+	    throw new IllegalArgumentException();
+	}
+	barcode = barcode.substring(1,31);
+	String zip = "";
+	for(int x=0; x<=25; x+=5){
+	    int test = 0;
+	    if(barcode.substring(x,x+5).equals("||:::")){
+		zip += "0";
+		test ++;
+	    }
+	    if(barcode.substring(x,x+5).equals(":::||")){
+		zip += "1";
+		test ++;
+	    }
+	    if(barcode.substring(x,x+5).equals("::|:|")){
+		zip += "2";
+		test ++;
+	    }
+	    if(barcode.substring(x,x+5).equals("::||:")){
+		zip += "3";
+		test ++;
+	    }
+	    if(barcode.substring(x,x+5).equals(":|::|")){
+		zip += "4";
+		test ++;
+	    }
+	    if(barcode.substring(x,x+5).equals(":|:|:")){
+		zip += "5";
+		test ++;
+	    }
+	    if(barcode.substring(x,x+5).equals(":||::")){
+		zip += "6";
+		test ++;
+	    }
+	    if(barcode.substring(x,x+5).equals("|:::|")){
+		zip += "7";
+		test ++;
+	    }
+	    if(barcode.substring(x,x+5).equals("|::|:")){
+		zip += "8";
+		test ++;
+	    }
+	    if(barcode.substring(x,x+5).equals("|:|::")){
+		zip += "9";
+		test ++;
+	    }
+	    if(test == 0){
+		throw new IllegalArgumentException();
+	    }
+	}
+	int sum = 0;
+	for(int x=0; x<5; x++){
+	    sum += Integer.parseInt(zip.substring(x,x+1));
+	}
+	if(Integer.parseInt(zip.substring(5,6)) != sum % 10){
+	    throw new IllegalArgumentException();
+	}
+	return zip.substring(0,5);
     }
 }
 	
